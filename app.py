513acee447f92ac059303ccf760343e54539972c
Dilip Sharma <dilipsharma640@gmail.com>
2020-08-07 01:25:07 +0530

    Add drive upload and change html

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template         
-app = Flask(__name__)
+from fastapi import FastAPI, Request, UploadFile, Response, File
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-@app.route("/")
-def hello():
-    return render_template('home.html')
+app = FastAPI()
 
-app.run()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "id": id})
+
+@app.post("/scan/")
+async def scan(request: Request):
+    form = await request.form()
+    print(list(form.values())[0])
+    return Response(status_code=200)
